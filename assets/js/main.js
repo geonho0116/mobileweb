f690f3653ad0e9174ae11155e5931bd19f01105b
geonho0116 <geonho0116@naver.com>
2020-11-03 11:11:02 +0900

    init

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/*
+	Prologue by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$nav = $('#nav');
+		// Header.
+			$('#header')
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'header-visible'
+				});
+
+})(jQuery);
